7c0a9d4ba2ddfa6d967deebab9c7fd690d09f755
sukmin <52587290+min1321@users.noreply.github.com>
2021-11-10 09:50:25 +0900

    gdown if no file

--- new_inference.sh
+++ new_inference.sh
@@ -1,20 +1,26 @@
 cd BoolQ
 pip install transformers
-gdown https://drive.google.com/uc?id=1GBViK4o6Fm0L83zl9Atv2J2wdxvBnQV4
+if [ ! -f ./boolq_data_results.tar ]; then
+    gdown https://drive.google.com/uc?id=1OZaN9qVNFlEbk7GIpaoREcEObLttibFk
+fi
 tar -xvf boolq_data_results.tar
 cp ../new_test/BoolQ.tsv ./boolq_data_results/data/SKT_BoolQ_Test.tsv
 python inference.py
 cd ..
 
 cd Copa
-gdown https://drive.google.com/uc?id=1QB66EebwWP2XhgZFweG00NIu3QIBZ-hh
+if [ ! -f ./copa_data_results.tar ]; then
+    gdown https://drive.google.com/uc?id=1QB66EebwWP2XhgZFweG00NIu3QIBZ-hh
+fi
 tar -xvf copa_data_results.tar
 cp ../new_test/CoPA.tsv ./copa_data_results/data/SKT_COPA_Test.tsv
 python inference.py
 cd ..
 
 cd WIC
-gdown https://drive.google.com/uc?id=1DUaUTTl-YAwhZQmTHaLVHsPmA64dyQ75
+if [ ! -f ./wic_model.tar ]; then
+    gdown https://drive.google.com/uc?id=1DUaUTTl-YAwhZQmTHaLVHsPmA64dyQ75
+fi
 tar -xvf wic_model.tar
 cp ../new_test/WiC.tsv ./Data/NIKL_SKT_WiC_Test.tsv
 python inference.py
@@ -22,7 +28,9 @@ cd ..
 
 cd Cola
 pip install transformers==3.5.1
-gdown https://drive.google.com/uc?id=1d-eIMrLZSxreeiE-vcO6lX5hjF70c6zS
+if [ ! -f ./cola_dataset_results.zip ]; then
+    gdown https://drive.google.com/uc?id=1d-eIMrLZSxreeiE-vcO6lX5hjF70c6zS
+fi
 unzip cola_dataset_results.zip
 cp ../new_test/CoLA.tsv ./cola_data_results/data/NIKL_CoLA_test.tsv
 python inference.py
