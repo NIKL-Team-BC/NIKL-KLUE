262da96f77359253363afa7c0c9359dc98ecd866
sukmin <52587290+min1321@users.noreply.github.com>
2021-11-10 09:49:50 +0900

    gdown if no file

--- all_inference.sh
+++ all_inference.sh
@@ -1,22 +1,30 @@
 cd BoolQ
 pip install transformers
-gdown https://drive.google.com/uc?id=1GBViK4o6Fm0L83zl9Atv2J2wdxvBnQV4
+if [ ! -f ./boolq_data_results.tar ]; then
+    gdown https://drive.google.com/uc?id=1OZaN9qVNFlEbk7GIpaoREcEObLttibFk
+fi
 tar -xvf boolq_data_results.tar
 python inference.py
 cd ..
 cd Copa
-gdown https://drive.google.com/uc?id=1QB66EebwWP2XhgZFweG00NIu3QIBZ-hh
+if [ ! -f ./copa_data_results.tar ]; then
+    gdown https://drive.google.com/uc?id=1QB66EebwWP2XhgZFweG00NIu3QIBZ-hh
+fi
 tar -xvf copa_data_results.tar
 python inference.py
 cd ..
 cd WIC
-gdown https://drive.google.com/uc?id=1DUaUTTl-YAwhZQmTHaLVHsPmA64dyQ75
+if [ ! -f ./wic_model.tar ]; then
+    gdown https://drive.google.com/uc?id=1DUaUTTl-YAwhZQmTHaLVHsPmA64dyQ75
+fi
 tar -xvf wic_model.tar
 python inference.py
 cd ..
 cd Cola
 pip install transformers==3.5.1
-gdown https://drive.google.com/uc?id=1d-eIMrLZSxreeiE-vcO6lX5hjF70c6zS
+if [ ! -f ./cola_dataset_results.zip ]; then
+    gdown https://drive.google.com/uc?id=1d-eIMrLZSxreeiE-vcO6lX5hjF70c6zS
+fi
 unzip cola_dataset_results.zip
 python inference.py
 cd ..
